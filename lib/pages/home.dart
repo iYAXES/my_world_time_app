98179ddda2e68110aa3deed509463d89aeaca8d4
iYAXES <iyaxes@gmail.com>
2023-10-23 10:25:01 +0100

    updated week format to previous

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,59 +1,95 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   Map data = {};
-
 
   @override
   Widget build(BuildContext context) {
-     data = data.isNotEmpty ? data : ModalRoute.of(context)?.settings.arguments as Map;
-     String bgImage = data['isDayTime'] ? 'day2.jpg' : 'night2.jpg';
+    data = data.isNotEmpty
+        ? data
+        : ModalRoute.of(context)?.settings.arguments as Map;
+    String bgImage = data['isDayTime'] ? 'day2.jpg' : 'night2.jpg';
 
     return Scaffold(
       backgroundColor: Colors.purple[600],
       body: Container(
-        decoration: BoxDecoration(image:
-        DecorationImage(image: AssetImage('assets/$bgImage'),fit: BoxFit.cover)),
-        child: SafeArea(child: Padding(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/$bgImage'),
+                fit: BoxFit.cover,
+                opacity: .88)),
+        child: SafeArea(
+            child: Padding(
           padding: const EdgeInsets.fromLTRB(0, 160, 0, 0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              ElevatedButton.icon(onPressed: () async{
-                dynamic result = await Navigator.pushNamed(context, '/location');
-                setState(() {
-                  data = {
-                    'location': result['location'],
-                    'time': result['time'],
-                    'flag': result['flag'],
-                    'isDayTime': result['isDayTime'],
-                    'weekday': result['weekday']
-                  };
-                });
-              },
-                  icon: Icon(Icons.edit_location, size: 25,),
-                  style: ElevatedButton.styleFrom(elevation: 0, backgroundColor: Colors.transparent),
-                  label: Text('Edit Location', style: TextStyle(fontSize: 18),)),
-              SizedBox(height: 15,),
+              ElevatedButton.icon(
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'location': result['location'],
+                        'time': result['time'],
+                        'flag': result['flag'],
+                        'isDayTime': result['isDayTime'],
+                        'weekday': result['weekday']
+                      };
+                    });
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    size: 25,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0, backgroundColor: Colors.transparent),
+                  label: Text(
+                    'Edit Location',
+                    style: TextStyle(fontSize: 18),
+                  )),
+              SizedBox(
+                height: 15,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                 CircleAvatar(backgroundImage: AssetImage('assets/${data['flag']}'),radius: 22,),
-                  SizedBox(width: 10,),
-                  Text(data['location'],style: TextStyle(fontSize: 40, fontFamily: 'Tektur', color: Colors.white),),
-
-              ],),
-              SizedBox(height: 20,),
-              Text(data['weekday'], style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),),
-              Text(data['time'], style: TextStyle(fontSize: 70, fontFamily: 'Moonrock', color: Colors.white),),
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/${data['flag']}'),
+                    radius: 22,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontFamily: 'Tektur',
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                data['weekday'],
+                style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                data['time'],
+                style: TextStyle(
+                    fontSize: 70, fontFamily: 'Moonrock', color: Colors.white),
+              ),
             ],
           ),
         )),
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -3,44 +3,38 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 
-class WorldTime{
-  
+class WorldTime {
   String? location;
   String? time;
   String? flag;
   String? url;
   String? weekday;
   bool? isDayTime;
-  
-     WorldTime({required this.location, required this.flag, required this.url});
-     
-      Future <void> getTime() async{
-        try{
-          Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-          Map data = jsonDecode(response.body);
 
-          String datetime = data['datetime'];
-          String offset = data['utc_offset'].substring(1,3);
-          int today = data['day_of_week'];
+  WorldTime({required this.location, required this.flag, required this.url});
 
+  Future<void> getTime() async {
+    try {
+      Response response =
+          await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
 
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      int today = data['day_of_week'];
 
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-          DateTime now = DateTime.parse(datetime);
-          now = now.add(Duration(hours: int.parse(offset)));
+      DateTime weekly = DateTime(today);
+      //weekly = weekly.subtract(new Duration(days: 1));
 
-          DateTime weekly = DateTime(today);
-          weekly = weekly.subtract(new Duration(days:1));
+      weekday = DateFormat('EEEE').format(weekly);
 
-          weekday = DateFormat('EEEE').format(weekly);
-
-          isDayTime = now.hour >6 && now.hour <20 ? true : false;
-          time = DateFormat.jm().format(now);
-
-        }catch(e){
-          print('caught the error: $e');
-        }
-
-      }
-  
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
+    } catch (e) {
+      print('caught the error: $e');
+    }
+  }
 }
